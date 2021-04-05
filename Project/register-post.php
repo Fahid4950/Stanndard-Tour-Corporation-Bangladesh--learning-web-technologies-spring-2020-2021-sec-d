6480886ebf1b482c8e8bf37a1d45cdb9cfa23430
Fahid4950 <fahid484950@gmail.com>
2021-04-05 13:58:18 +0600

    Project Work gerister post addition work

--- Project/register-post.php
+++ Project/register-post.php
@@ -12,10 +12,19 @@ $name = $_POST['fname'];
 $username = $_POST['fusername'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$gender = $_POST['gender'];
+//$dob = $_POST['year'] . '/' . $_POST['month'] . '/' . $_POST['day'];
 
-$insert = "INSERT INTO users(name,username,email,password) VALUES('$name','$username' ,'$email',$password)";
+//$dob = $_POST['dob'];
+//$type = $_POST['type'];
+date_default_timezone_set('Asia/Dhaka');
+$created_at = date('Y-m-d h:i:s');
+$insert = "INSERT INTO users(name,username,email,password,gender,created_at) VALUES('$name','$username' ,'$email','$password','$gender','$created_at')";
+
+// $insert = "INSERT INTO users(name,username,email,password,gender,dob) VALUES('$name','$username' ,'$email','$password','$gender',$dob)";
 
 $insert_result = mysqli_query($db_connect,$insert);
-header('location:inter.php');
+$success = 'Registration Successfull!!!';
 
+ header('location:register.php?success='.$success);
  ?>
